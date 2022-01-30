b80e5b1f9c7d4ef091073dcb02f642eac730241f
Izadora Sant'Anna <santannaizadora@gmail.com>
2022-01-30 15:28:23 -0300

    tela para confirmar pedido

--- js/script.js
+++ js/script.js
@@ -3,21 +3,24 @@ const mainDish = [
         id: 1,
         type: 'dish',
         image: 'img/frango_yin_yang 1.png',
-        name: ' Frango Yin-Yang Completo', price: '19,90',
+        name: ' Frango Yin-Yang', 
+        price: 19.90,
         description: 'Um pouco de batata, um pouco de salada'
     },
     {
         id: 2,
         type: 'dish',
         image: 'img/frango_yin_yang 1.png',
-        name: ' Frango Yin-Yang com batata', price: '9,90',
+        name: ' Frango Yin-Yang com batata', 
+        price: 9.90,
         description: 'Um pouco de batata'
     },
     {
         id: 3,
         type: 'dish',
         image: 'img/frango_yin_yang 1.png',
-        name: ' Frango Yin-Yang com salada', price: '10,90',
+        name: ' Frango Yin-Yang com salada', 
+        price: 10.90,
         description: 'Um pouco de salada'
     },
 ];
@@ -26,21 +29,24 @@ const drinks = [
         id: 4,
         type: 'drink',
         image: 'img/coquinha_gelada 2.png',
-        name: ' Frango Yin-Yang Completo', price: '19,90',
+        name: ' Coca', 
+        price: 19.90,
         description: 'Um pouco de batata, um pouco de salada'
     },
     {
         id: 5,
         type: 'drink',
         image: 'img/coquinha_gelada 2.png',
-        name: ' Frango Yin-Yang com batata', price: '9,90',
+        name: ' Fanta', 
+        price: 9.90,
         description: 'Um pouco de batata'
     },
     {
         id: 6,
         type: 'drink',
         image: 'img/coquinha_gelada 2.png',
-        name: ' Frango Yin-Yang com salada', price: '10,90',
+        name: ' Pepsi',
+        price: 10.90,
         description: 'Um pouco de salada'
     },
 ];
@@ -49,23 +55,32 @@ const desserts = [
         id: 7,
         type: 'dessert',
         image: 'img/pudim 2.png',
-        name: ' Frango Yin-Yang Completo', price: '19,90',
+        name: ' Cookie', 
+        price: 19.90,
         description: 'Um pouco de batata, um pouco de salada'
     },
     {
         id: 8,
         type: 'dessert',
         image: 'img/pudim 2.png',
-        name: ' Frango Yin-Yang com batata', price: '9,90',
+        name: ' Pudim', 
+        price: 9.90,
         description: 'Um pouco de batata'
     },
     {
         id: 9, type: 'dessert',
         image: 'img/pudim 2.png',
-        name: ' Frango Yin-Yang com salada', price: '10,90',
+        name: ' Sorvete', 
+        price: 10.90,
         description: 'Um pouco de salada'
     },
 ];
+
+const pedido = {
+    pratoPrincipal: null,
+    bebida: null,
+    sobremesa: null
+}
 
 
 $(document).ready(function () {
@@ -79,45 +94,95 @@ const loadProducts = (products, idElement) => {
     let contentProducts = "";
     products.forEach(item => {
         contentProducts +=
-            `<div id="${item.id}" class="produto" onclick="selectProducts('${item.type}',${item.id})" data-identifier="food-option" >   
+            `<div id="${item.id}" class="produto produto-${item.type}" onclick="selectProducts('${item.type}', ${item.id})" data-identifier="food-option" >   
                 <img src="${item.image}">
                 <h4 data-identifier="food-title"> ${item.name}</h4>
                 <p class="descricao">${item.description}</p>
-                <p class="preco" data-identifier="food-price">R$ ${item.price}</p>
+                <p class="preco" data-identifier="food-price">R$ ${parsePriceToString(item.price)}</p>
                 <ion-icon class="escondido" name="checkmark-circle"></ion-icon> 
             </div>`;
     });
 
     document.getElementById(idElement).innerHTML = contentProducts;
-}
+};
 
 
-const selectProducts = (classePrato, idElement) => {
-    const selecionado = document.querySelector(`.${classePrato} .selecionado`)
+const selectProducts = (typeProduct, idProduct) => {
+    const selecionado = document.querySelector(`.${typeProduct} .selecionado`);
+    let check;
+    
     if (selecionado !== null) {
-        const check = document.querySelector(`.${classePrato} .selecionado ion-icon`)
+        check = document.querySelector(`.${typeProduct} .selecionado ion-icon`)
         selecionado.classList.remove("selecionado");
         check.classList.add("escondido");
-    } 
-        const SelectedItem = document.getElementById(idElement);
-        SelectedItem.classList.add("selecionado");
-        const check = document.querySelector(`.${classePrato} .selecionado ion-icon`)
-        check.classList.remove("escondido");
+    }
 
-        habilitarConfirmar();
+    const selectedItem = document.getElementById(idProduct);
+    selectedItem.classList.add("selecionado");
+    check = document.querySelector(`.${typeProduct} .selecionado ion-icon`)
+    check.classList.remove("escondido");
+
+    addProductToOrder(typeProduct, idProduct);
+
+    habilitarConfirmar();
+};
+
+const addProductToOrder = (typeProduct, idProduct) => {
+    let product;
+
+    switch(typeProduct) {
+        case 'dish':
+            product = mainDish.find(item => item.id === idProduct);
+            pedido.pratoPrincipal = product;
+            break;
+        case 'drink':
+            product = drinks.find(item => item.id === idProduct);
+            pedido.bebida = product;
+            break;
+        case 'dessert':
+            product = desserts.find(item => item.id === idProduct);
+            pedido.sobremesa = product;
+            break;
+    };
+
+
+};
+
+const fecharPedido = () => {
+    
+};
+
+const habilitarConfirmar = () =>{
+    const selecionados = document.querySelectorAll(".selecionado");
+
+    if(selecionados.length === 3){
+        const botaoDesabilitado = document.querySelector(".botao-desabilitado");
+        const botaoHabilitado = document.querySelector(".botao-habilitado");
+        botaoDesabilitado.classList.add("escondido");
+        botaoHabilitado.classList.remove("escondido");
+    }
+};
+
+const confirmarPedido = () => {
+    const listaPedido = document.getElementById('listaPedido');
+
+    listaPedido.innerHTML = `
+        <div> <span>${pedido.pratoPrincipal.name}</span>  <span>${parsePriceToString(pedido.pratoPrincipal.price)}</span> </div>
+        <div> <span>${pedido.bebida.name}</span>    <span>${parsePriceToString(pedido.bebida.price)}</span> </div>
+        <div> <span>${pedido.sobremesa.name}</span>  <span> ${parsePriceToString(pedido.sobremesa.price)}</span></div>
+        
+        <div><span class="bold">TOTAL</span>  <span class="bold">R$  ${parsePriceToString((pedido.pratoPrincipal.price + pedido.bebida.price + pedido.sobremesa.price))}</span></div>
+    `;
+
+    let janela = document.querySelector(".confirmar-pedido");
+    janela.classList.remove("escondido");
+};
+
+const cancelar = () => {
+    let janela = document.querySelector(".confirmar-pedido");
+    janela.classList.add("escondido");
 }
 
-function habilitarConfirmar(){
-    let botao = document.querySelector(".botao");
-    if(botao.classList.contains("desabilitado")){
-        let selecionados = document.querySelectorAll(".selecionado");
-        if(selecionados.length == 3){
-
-            botao.innerHTML = "Fechar pedido";
-            botao.classList.remove("desabilitado");
-            botao.classList.add("habilitado");
-            botao.disabled = false;
-        }
-        
-    }
+const parsePriceToString = (value) => {
+    return value.toFixed(2).toString().replace(".",",");
 }
